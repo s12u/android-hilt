7b68efca94f93cbfd6228fc385c93a02e5515973
Subin Nam <onlybiny@gmail.com>
2020-10-18 22:14:26 +0900

    Complete codelab

--- app/src/main/java/com/example/android/hilt/LogApplication.kt
+++ app/src/main/java/com/example/android/hilt/LogApplication.kt
@@ -17,13 +17,15 @@
 package com.example.android.hilt
 
 import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
 
+@HiltAndroidApp
 class LogApplication : Application() {
 
-    lateinit var serviceLocator: ServiceLocator
+//    lateinit var serviceLocator: ServiceLocator
 
     override fun onCreate() {
         super.onCreate()
-        serviceLocator = ServiceLocator(applicationContext)
+//        serviceLocator = ServiceLocator(applicationContext)
     }
 }
